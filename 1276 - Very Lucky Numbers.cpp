df83c53eceee8648381b63fb8bec5b3124093abd
Md Monirujjaman <55328066+mdmonir1997@users.noreply.github.com>
2020-03-14 21:18:29 +0600

    1276 - Very Lucky Numbers
    http://lightoj.com/volume_showproblem.php?problem=1276

---
+++ 1276 - Very Lucky Numbers.cpp
@@ -0,0 +1,53 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+ 
+const ll lim=1e12;
+vector<ll>v;
+vector<ll>lucky;
+ 
+void fun(ll n){
+    if(n>lim) return;
+    if(n!=0) v.push_back(n);
+    fun(n*10+4);
+    fun(n*10+7);
+}
+
+void num(int l,ll h,int size){
+ 
+    if(l>=size) return;
+    for(int i=l;i<size;i++){
+        ll t=h*v[i];
+        if(t>=lim || t<=0) return;
+        lucky.push_back(t);
+        num(i,t,size);
+    }
+}
+ 
+int main(){
+ 
+    fun(0);
+    sort(v.begin(),v.end());
+    int s=v.size();
+ 
+    num(0,1,s);
+
+    set<ll> st(lucky.begin(),lucky.end());
+    lucky.assign(st.begin(),st.end());
+ 
+    int t,cas=1;
+    ll a,b;
+ 
+    cin>>t;
+ 
+    while(t--){
+        cin>>a>>b;
+ 
+        int l=lower_bound(lucky.begin(),lucky.end(),a)-lucky.begin();
+        int r=upper_bound(lucky.begin(),lucky.end(),b)-lucky.begin();
+ 
+        cout<<"Case "<<cas++<<": "<<r-l<<endl;
+    }
+ 
+ 
+}
